6c4cfdae04db6ac1a5db41d76aa562d9c406585f
mjanes71 <mjanes71@gmail.com>
2022-10-04 11:06:33 -0400

    eod progress

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func main(){
 	fmt.Println(Hello("Megan"))
 }
 
 func Hello(name string) string{
-	return fmt.Sprintf("Hello, %v!", name)
+	return fmt.Sprintf(englishHelloPrefix + "%v!", name)
 }
