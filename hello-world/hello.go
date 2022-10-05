c4548f4027d5a5d37a61cff6ed92f374faa9b425
mjanes71 <mjanes71@gmail.com>
2022-10-05 11:45:05 -0400

    final work

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,14 +3,32 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func main(){
-	fmt.Println(Hello("Megan"))
+func main() {
+	fmt.Println(hello("Megan", "English"))
 }
 
-func Hello(name string) string{
+func hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return fmt.Sprintf(englishHelloPrefix + "%v!", name)
+
+	return fmt.Sprintf(greetingPrefix(language)+"%v!", name)
+}
+
+//named return values, like this one prefix, create
+//a variable in your function with that name and type
+//its assigned a zero value/empty string by default
+func greetingPrefix(language string) (prefix string){
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "French":
+		prefix = frenchHelloPrefix
+	default: 
+		prefix = englishHelloPrefix
+	}
+	return 
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -1,24 +1,35 @@
 package main
+
 import "testing"
 
-func TestHello(t *testing.T){
+func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Megan")
+		got := hello("Megan", "English")
 		want := "Hello, Megan!"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := hello("", "English")
 		want := "Hello, World!"
 
 		assertCorrectMessage(t, got, want)
 	})
+	t.Run("in spanish", func(t *testing.T) {
+		got := hello("Esperanza", "Spanish")
+		want := "Hola, Esperanza!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in french", func(t *testing.T) {
+		got := hello("Renee", "French")
+		want := "Bonjour, Renee!"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
-//this is a helper function in the test suite that you
-//can feed into the main tests
-func assertCorrectMessage(t testing.TB, got, want string){
+// this is a helper function in the test suite that you
+// can feed into the main tests
+func assertCorrectMessage(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
